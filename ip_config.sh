9726a84750ddb35c37680627d21688197516398f
Sam Cofer <samcofer@gmail.com>
2023-10-06 14:23:35 -0500

    move old netplan away

--- ip_config.sh
+++ ip_config.sh
@@ -7,4 +7,5 @@ cat /etc/netplan/00-installer-config.yaml
 cp /etc/netplan/00-installer-config.yaml /etc/netplan/01-custom-config.yaml
 sed -i "s=10.10.10.71/24=$MYIP=g" /etc/netplan/01-custom-config.yaml
 cat /etc/netplan/01-custom-config.yaml
+mv /etc/netplan/00-installer-config.yaml /root/
 netplan apply
