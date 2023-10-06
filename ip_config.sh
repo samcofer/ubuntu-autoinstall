377ba811d45b5df8c151d02e92621fc4cc82a11c
Sam Cofer <samcofer@gmail.com>
2023-10-06 09:46:12 -0500

    Add Logging and output redirection

--- ip_config.sh
+++ ip_config.sh
@@ -3,5 +3,7 @@
 
 for i in {61..69}; do ip="10.10.10.$i"; ping -c 1 -W 1 $ip &>/dev/null ||  { export MYIP="$ip/24"; break; }; done
 echo $MYIP
+cat /etc/netplan/00-installer-config.yaml
 sed -i "s=10.10.10.71/24=$MYIP=g" /etc/netplan/00-installer-config.yaml
+cat /etc/netplan/00-installer-config.yaml
 netplan apply
