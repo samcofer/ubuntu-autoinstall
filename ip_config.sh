ec2a918ad07b00904f8e447b586d87db76d42dcc
Sam Cofer <samcofer@gmail.com>
2023-10-06 10:05:23 -0500

    New config file

--- ip_config.sh
+++ ip_config.sh
@@ -4,6 +4,7 @@
 for i in {61..69}; do ip="10.10.10.$i"; ping -c 1 -W 1 $ip &>/dev/null ||  { export MYIP="$ip/24"; break; }; done
 echo $MYIP
 cat /etc/netplan/00-installer-config.yaml
-sed -i "s=10.10.10.71/24=$MYIP=g" /etc/netplan/00-installer-config.yaml
-cat /etc/netplan/00-installer-config.yaml
+cp /etc/netplan/00-installer-config.yaml /etc/netplan/01-custom-config.yaml
+sed -i "s=10.10.10.71/24=$MYIP=g" /etc/netplan/01-custom-config.yaml
+cat /etc/netplan/01-custom-config.yaml
 netplan apply
