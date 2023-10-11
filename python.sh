0f32ba5bafbc700d5aa6c3192403d55c1b417c0f
Sam Cofer <samcofer@gmail.com>
2023-10-11 15:52:57 -0500

    Add python install to server cloning to enable ansible and add history time formatting to all servers.

--- python.sh
+++ python.sh
@@ -6,7 +6,7 @@ sudo apt-get install gdebi-core -y
 PYTHON_VERSION="3.11.3"
 
 curl -O https://cdn.rstudio.com/python/ubuntu-2204/pkgs/python-${PYTHON_VERSION}_1_amd64.deb
-sudo gdebi -n python-${PYTHON_VERSION}_1_amd64.deb -y
+sudo gdebi -n python-${PYTHON_VERSION}_1_amd64.deb
 
 /opt/python/"${PYTHON_VERSION}"/bin/pip install --upgrade \
     pip setuptools wheel
